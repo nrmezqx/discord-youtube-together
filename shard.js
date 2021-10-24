3ce84447c47bf5cc1e82f1fa2f17c6369057af94
pixdâynusxd <78669178+nrmezqx@users.noreply.github.com>
2021-10-24 17:59:19 +0300

    Create shard.js

---
+++ shard.js
@@ -0,0 +1,12 @@
+const { ShardingManager } = require('discord.js');
+
+const bumbe = new ShardingManager('./index.js', { //main
+	totalShards: 1, //1,2,auto, (1 Değişme. -nomik)
+});
+bumbe.spawn();
+
+bumbe.on('shardCreate', shard => {
+    console.log(`${shard.id} İDli shard başlatıldı bot artık Yanak gibi!`);
+});
+
+//v12.init
